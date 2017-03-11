3fa1477149db46e180c9d8a752d9c14bc2b89d34
KashishKashish <kashishkashish1000@gmail.com>
2017-03-12 01:00:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
  // now adding 3 more URL handlers  and these are only text responses not html responses 
  app.get('/article-one',function(req,res){
-     res.send('Article one requested and will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
      
  });
  app.get('/article-two',function(req,res){
