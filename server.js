7d7fa0842a8b8079cca8802e63b82d0c10b2281a
KashishKashish <kashishkashish1000@gmail.com>
2017-03-13 12:08:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,60 +4,77 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-var articleOne={
+var articles={
+    articleOne:{
     title:'Article One| Kashish',
     heading:'Article One',
     date: '12 March,2017',
-    content:`<p>
-            This were I started to write some content in my first WebApp.
-        </p>
-        <p>
-            This were I started to write some content in my first WebApp.
-        </p>
-        <p>
-            This were I started to write some content in my first WebApp.
-        </p>
+    content:
+            `
+                <p>
+                This were I started to write some content in my first WebApp.
+                </p>
+                <p>
+                    This were I started to write some content in my first WebApp.
+                </p>
+                <p>
+                    This were I started to write some content in my first WebApp.
+                </p>
+           `},  // use back quotes on both side
+    articleTwo:{title:'Article Two| Kashish',
+    heading:'Article Two',
+    date: '13 March,2017',
+    content:
+            `
+             <p>This is the article 2 content on server.js file.</p>
+           `},
+    articleThree:{title:'Article Three| Kashish',
+    heading:'Article Three',
+    date: '14 March,2017',
+    content:
+            `
+        <p>This is the article 3 content on server.js file.</p>
+           `}
     
-`};  // use back quotes on both sides
+};
 // we will inject the data into the htmltemplate
-
+// takes the object , extract the right variables ,make a string, return the string
 function createTemplate(data){
         var title=data.title;
         var heading=data.heading;
         var date=data.date;
         var content=data.content;
         var htmlTemplate=
-`
-     <html>
-        <head>
-            <title>
-                ${title}
-            </title>
-    <meta name="viewport" content="width=device-width,initial-scale=1"/>  
-    
-    <!--tag for mobile devices, it shows zoomed view sutiable to view in mobile devices--> 
-     
-     <link href="/ui/style.css" rel="stylesheet" />
-
-        </head>
-        <div class="container">
-            <div>
-                <a href="/">Home</a>
-            </div>
-            <hr>
-        <h3>
-            ${heading}
-        </h3>
-            <div>
-                ${date}
-            </div>
-            <div>
-            ${content}
-            </div>
-        </div>
-    </html>
-`;    
+                    `
+                         <html>
+                            <head>
+                                <title>
+                                    ${title}
+                                </title>
+                        <meta name="viewport" content="width=device-width,initial-scale=1"/>  
+                        
+                        <!--tag for mobile devices, it shows zoomed view sutiable to view in mobile devices--> 
+                         
+                         <link href="/ui/style.css" rel="stylesheet" />
+                    
+                            </head>
+                            <div class="container">
+                                <div>
+                                    <a href="/">Home</a>
+                                </div>
+                                <hr>
+                            <h3>
+                                ${heading}
+                            </h3>
+                                <div>
+                                    ${date}
+                                </div>
+                                <div>
+                                ${content}
+                                </div>
+                            </div>
+                        </html>
+                    `;    
     return htmlTemplate; 
 }
  //line 8,12,16 handles specific urls
@@ -79,15 +96,17 @@ app.get('/ui/madi.png', function (req, res) {
 // & now chaning to text responses to showing a webpage
  app.get('/article-one',function(req,res){
   //  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   // /article-one is replaced by /:articleName 
+   // /:articleName== article-one, by adding colan : it converts into a variable.
     res.send(createTemplate(articleOne)); 
  });
  app.get('/article-two',function(req,res){
-     res.sendFile(path.join(__dirname,'ui','article-two.html'));
-     
+    res.send(createTemplate(articleTwo)); 
  });
  app.get('/article-three',function(req,res){
-     res.sendFile(path.join(__dirname,'ui','article-three.html'));
- });
+   //  res.sendFile(path.join(__dirname,'ui','article-three.html'));
+res.send(createTemplate(articleThree));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
